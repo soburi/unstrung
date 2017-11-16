2c107856a3710c72c1352754484d661dada28681
soburi <soburi@gmail.com>
2017-11-17 07:28:33 +0900

    delete

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(unstrung):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="unstrung" command="/usr/bin/unstrung"
--- debian/unstrung.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the unstrung package
-#
-0 4	* * *	root	[ -x /usr/bin/unstrung_maintenance ] && /usr/bin/unstrung_maintenance
